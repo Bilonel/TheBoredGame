8c0118f73202092dc162e569f4228ed69ed8a792
Bilonel <bilaldemir.3236@gmail.com>
2022-04-11 13:24:46 +0300

    Added About Form

---
+++ OOP_Lab_II/Menus_Forms/Form_About.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Lab_II.Menus_Forms
+{
+    public partial class Form_About : Form
+    {
+        public Form_About()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
