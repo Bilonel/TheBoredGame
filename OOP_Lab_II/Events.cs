7a6b9d99c17c6f4cb8113ef47687d0c1ec4d08cd
Bilonel <bilaldemir.3236@gmail.com>
2022-04-25 06:45:03 +0300

    Added Textures and Events

---
+++ OOP_Lab_II/Events.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace GAME_TEST
+{
+    class Events
+    {
+        public List<EventHandler> events { get; }
+        private Events()
+        {
+            events = new List<EventHandler>();
+            
+        }
+        private static Events Instance;
+        public static Events instance
+        {
+            get
+            {
+                if (Instance == null)
+                    Instance = new Events();
+                return Instance;
+            }
+        }
+    }
+}
